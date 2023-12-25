fb5f1fcf47255d48b4d3bcb001fd6341cd6b5713
gfes980617a <10010403+gfes980617a@users.noreply.github.com>
2023-12-26 05:00:30 +0800

    Add files via upload

--- sql-db.js
+++ sql-db.js
@@ -1,56 +1,45 @@
+var dev_mode = true;
 var db;
-var db_div = document.getElementById("lost_camera_db");
 var db_file = "/lost_camera_db/database.sqlite";
-function init_sqljs() {
-    initSqlJs().then(function (SQL) {
-        document.getElementById("db_load_button").value = "載入中...";
-        document.getElementById("db_load_button").disabled = true;
-        const xhr = new XMLHttpRequest();
-        if (db_file = db_div.getAttribute("db_file"));
-        console.log("load db_file:", db_file);
-        xhr.open('GET', "/lost_camera_db/database.sqlite", true);
-        xhr.responseType = 'arraybuffer';
-        xhr.onload = e => {
-            const uInt8Array = new Uint8Array(xhr.response);
-            db = new SQL.Database(uInt8Array);
-            document.getElementById("db_date").innerHTML = db.exec("SELECT `date` FROM `main` WHERE `ref`='我'")[0].values[0][0];
-            document.getElementById("db_count").innerHTML = db.exec("SELECT count() as 'count' FROM `main`")[0].values[0][0];
-            Array.from(document.getElementsByClassName("search")).forEach(element => {
-                element.disabled ^= true;
-            });
-            document.getElementById("db_load_button").value = "資料庫ok!";
-        };
-        xhr.send();
-    });
-}
+var search_opt = { "fast": false };
 function make_table(div_id) {
     db_div = document.getElementById(div_id);
-    db_div.innerHTML += " \
-    <h2>遺失器材(相機/鏡頭/配件)序號列表</h2> \
-    <div id=\"db_load\"> \
-        <a href=\"https://github.com/gfes980617a/lost_camera_db\">資料庫@github</a> \
-        <input id='db_load_button' style='width:7em;' type='button' onclick='init_sqljs()' value='載入資料庫' /> \
-        <div>最後更新時間<span id='db_date'>0000-00-00</span>共<span id='db_count'>-?-</span>筆資料</div> \
-    </div> \
-    <table align='center' border='1' style='text-align:center;'> \
-        <thead> \
-            <tr><td>品牌</td><td>名稱</td><td>序號</td><td>日期</td><td>來源</td></tr> \
-            <tr onchange='search_function()'> \
-                <td style='width:25%'><input type='text' class='search' id='brand' disabled></td> \
-                <td style='width:25%'><input type='text' class='search' id='model' disabled></td> \
-                <td style='width:25%'><input type='text' class='search' id='sn' disabled></td> \
-                <td style='width:25%'><input type='text' class='search' id='date' value='2023-' disabled></td> \
-                <td style='width:25%'><input type='text' class='search' id='ref' disabled></td> \
-            </tr> \
-            <tr> \
-                <td colspan='4'>找到<span id='db_search_count'>-?-</span>個符合的項目</td> \
-                <td><input id='db_search_button' class='search' type='button' onclick='search_function()' value='搜尋' disabled/></td> \
-            </tr> \
-        </thead> \
-        <tbody id='db_search_body'></tbody> \
-    </table>";
+    db_div.innerHTML += `
+    <h2>遺失器材(相機/鏡頭/配件)序號列表</h2>
+    <div id=\"db_load\">
+        <a href=\"https://github.com/gfes980617a/lost_camera_db\" target=\"_blank\">原始碼+資料庫@github</a>
+        <div id='db_status'>載入中......</div>
+    </div>
+    <form><table align='center' border='1' style='text-align:center;'>
+        <thead>
+            <tr><td>品牌</td><td>名稱</td><td>序號</td><td>日期</td><td>來源</td></tr>
+            <tr onchange='{if(!search_opt.fast)search_exec()}' onkeyup='{if(search_opt.fast)search_exec()}'>
+                <td style='width:25%'><input type='text' class='search' id='brand' disabled></td>
+                <td style='width:25%'><input type='text' class='search' id='model' disabled></td>
+                <td style='width:25%'><input type='text' class='search' id='sn' disabled></td>
+                <td style='width:25%'><input type='text' class='search' id='date' disabled></td>
+                <td style='width:25%'><input type='text' class='search' id='ref' disabled></td>
+            </tr>
+            <tr>
+                <td colspan='4'>
+                    <input type='checkbox' id='search_fast' onclick='search_option()' checked disabled><label for='search_fast'>不用按ENTER的快速搜尋模式，顯示大量資料時請小心使用</label><br>
+                    <input type='checkbox' id='search_xxx' disabled><label for='search_fast'>xxx模式</label><br>
+                </td>
+                <td><input id='db_search_button' type='button' onclick='search_exec()' value='Search' disabled/></td>
+            </tr>
+            <tr>
+                <td colspan='4'><code id='sql_code'>SELECT * FROM \`main\` WHERE 1</code></td>
+                <td>找到<span id='db_search_count'>??</span>個項目</td>
+            </tr>
+        </thead>
+        <tbody id='db_search_body'></tbody>
+    </table></form>`;
 }
-function search_function() {
+function search_option() {
+    search_opt["fast"] = document.getElementById("search_fast").checked;
+    // search_opt["fast"] = document.getElementById("search_fast").checked;
+}
+function search_exec() {
     function append_table(row) {
         let tr = "<tr>";
         tr += "<td>" + row["brand"] + "</td>";
@@ -61,52 +50,57 @@ function search_function() {
         tr += "</tr>";
         return tr;
     }
-
-    let input = document.getElementsByClassName("search");
-    let brand = input["brand"].value;
-    let model = input["model"].value;
-    let sn = input["sn"].value;
-    let date = input["date"].value;
-    let ref = input["ref"].value;
-    console.log("Search:",brand,model,sn,date,ref);
-    { // count
-        var stmt = db.prepare("SELECT count(`sn`) as 'count' FROM `main`WHERE 1\
-        AND `brand` like $brand \
-        AND `model` like $model \
-        AND `sn`    like $sn \
-        AND `date`  like $date \
-        AND `ref`   like $ref \
-        ORDER BY`date` DESC; ");
-        stmt.bind({
-            $brand: "%" + brand + "%",
-            $model: "%" + model + "%",
-            $sn: "%" + sn + "%",
-            $date: "%" + date + "%",
-            $ref: "%" + ref + "%",
-        });
-        if (stmt.step()) {
-            document.getElementById("db_search_count").innerHTML = stmt.getAsObject()['count'];
-        }
+    function sql_string() {
+        var sql = "SELECT * FROM \`main\` WHERE 1 \n";
+        Array.from(document.getElementsByClassName("search")).forEach(element => {
+            if (element.value) { // if input not null, add to sql
+                sql += `AND \`${element.id}\` like '%${element.value}%' `
+            }
+        })
+        sql += "\nORDER BY \`date\` DESC; ";
+        return sql;
     }
-    { // rows
-        var stmt = db.prepare("SELECT * FROM `main` WHERE 1\
-        AND `brand` like $brand \
-        AND `model` like $model \
-        AND `sn`    like $sn \
-        AND `date`  like $date \
-        AND `ref`   like $ref \
-        ORDER BY`date` DESC; ");
-        stmt.bind({
-            $brand: "%" + brand + "%",
-            $model: "%" + model + "%",
-            $sn: "%" + sn + "%",
-            $date: "%" + date + "%",
-            $ref: "%" + ref + "%",
-        });
-        let tbody = document.getElementById("db_search_body");
-        tbody.innerHTML = "";
-        while (stmt.step()) {
-            tbody.innerHTML += append_table(stmt.getAsObject());
-        }
+    var count = db.exec(sql_string().replace("*", "count()"))[0].values[0][0];
+    document.getElementById("db_search_count").innerHTML = count;
+
+    var stmt = db.prepare(sql_string());
+    document.getElementById("sql_code").innerHTML = stmt.getSQL().replace("\n\n", "\n").replaceAll("\n", "<br>");
+    let tbody = document.getElementById("db_search_body");
+    tbody.innerHTML = "";
+    while (stmt.step()) {
+        tbody.innerHTML += append_table(stmt.getAsObject());
     }
 }
+function init_sqldb() {
+    if (document.getElementById("lost_camera_db") == undefined)
+        return 0;
+    make_table("lost_camera_db");
+    var script = document.createElement("script");
+    script.src = "/lost_camera_db/sql.js";
+    script.onload = () => {
+        initSqlJs().then(function (SQL) {
+            if (db_div.getAttribute("db_file")) {
+                db_file = db_div.getAttribute("db_file");
+            }
+            console.log("db file:", db_file);
+            fetch(db_file, { mode: 'no-cors' }).then(function (response) {
+                return response.arrayBuffer();
+            }).then(function (response) {
+                db = new SQL.Database(new Uint8Array(response));
+
+                const db_date = db.exec("SELECT `date` FROM `main` WHERE `ref`='我'")[0].values[0][0];
+                const db_count = db.exec("SELECT count() FROM `main`")[0].values[0][0];
+                document.getElementById("db_status").innerHTML = "最後更新時間" + db_date + " 共" + db_count + "筆資料";
+                console.log("db status:", db_date, ",", db_count);
+
+                Array.from(document.getElementsByTagName("input")).forEach(element => {
+                    element.disabled ^= true;
+                });
+                search_option();
+                // search_exec();
+            });
+        });
+    };
+    window.document.head.appendChild(script);
+};
+init_sqldb();
